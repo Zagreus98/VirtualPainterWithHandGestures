d6dc81497b1689c7d1d857043830dcd98e446a88
Andrei Alexandru <alex.ombla@gmail.com>
2022-04-20 20:51:48 +0300

    Added thershold for prediction

--- model/keypoint_classifier/keypoint_classifier.py
+++ model/keypoint_classifier/keypoint_classifier.py
@@ -3,6 +3,7 @@
 import numpy as np
 import torch.nn as nn
 import torch
+import torch.nn.functional as F
 
 class KeyPointClassifier():
     def __init__(self,model_path='model/keypoint_classifier/keypoint_classifier.pt',num_classes = 4):
@@ -15,14 +16,19 @@ class KeyPointClassifier():
                         nn.Linear(10,num_classes)
         )
         self.model.load_state_dict(torch.load(model_path,map_location=torch.device('cpu')))
+        self.score_th = 0.5
 
     def __call__(self, landmark_list):
         self.model.eval()
         landmark_list = torch.tensor(landmark_list).unsqueeze(0)
         with torch.no_grad():
             scores = self.model(landmark_list.float())
+            scores = F.softmax(scores,dim=1)
             result_index = torch.argmax(scores,dim=1)
 
-            return result_index.item()
+            if scores.squeeze(0)[result_index] < self.score_th:
+                return 0
+            else:
+                return result_index.item()
 
 # TODO : adauga threshold pentru predictii
